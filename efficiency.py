2fc3a54e586ecf34eca70473cca08d48b9868108
Marina Amorin <mamorim@drury.edu>
2022-03-04 10:31:10 -0600

    ok

--- efficiency.py
+++ efficiency.py
@@ -18,13 +18,12 @@ import timeit
 # expression to get the execution time of
 # the function, number defines how many
 # times we execute this function
-input_array =[[100,70,.95],[1000,800,.80],[50,40,.80]]
-binary_search_time=[]
+input_array =[[1000000000000,800000000000,.90],[100,70,.95],[50,40,.80]]
+findSmallest_time=[]
 
 for i in range (len(input_array)):
-    print("inside loop")
-    binary_search_time.append(timeit.timeit(lambda:binary_search_updated2(input_array[i][0],input_array[i][1],input_array[i][2]), number=1))
-    print(binary_search_time)#binary_search_time = timeit.timeit(lambda:binary_search(100,70,.95), number=100)
+    findSmallest_time.append(timeit.timeit(lambda:findSmallest_1(input_array[i][0],input_array[i][1],input_array[i][2]), number=20))
+    #print(finSammlest_time)#binary_search_time = timeit.timeit(lambda:binary_search(100,70,.95), number=100)
     
 # printing the execution time
-print(binary_search_time)
+print(findSmallest_time)
