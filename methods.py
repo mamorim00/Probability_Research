c48a68e22f21810176c610efc411376866cdbde6
Marina Amorin <mamorim@drury.edu>
2022-03-04 10:20:03 -0600

    not fixed yet

--- methods.py
+++ methods.py
@@ -195,10 +195,10 @@ def printProbability(M,n):
 
 
 def probIsOne(M,n,w):
-    return True if M*w == n else False
+    return True if M*w <= n else False
 
 
 # Error when the number is close to 1
 
-print(binary_search_updated2(1000,800,.9))
-print("RIGHT ONE",findSmallest_1(1000,800,.9))
+print(binary_search_updated2(1000,800,.70))
+print("RIGHT ONE",findSmallest_1(1000,800,.70))
