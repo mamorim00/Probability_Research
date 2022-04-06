a32843b190146fd50b3419c06837a07ee7229dbf
Marina Amorin <mamorim@drury.edu>
2022-04-06 09:48:59 -0500

    updated

--- findsmallest.py
+++ findsmallest.py
@@ -8,19 +8,21 @@ from methods import binary_search,binary_search_updated,findSmallest_1,findSmall
 def main():
 
     #Number_pop: is the population size.
-    number_population = int(input("Choose the population size:"))
+    number_population = int(input("Choose the population size: "))
 
     #Population_s: is the number of successes in the population.
-    population_s = int(input("Choose the number os successes in the population:"))
-    
+    population_s = float(input("Choose the probability of votes for A from 0.5 to 1: "))
+    while (population_s<.50 or population_s>1):
+        print("Please enter a value between 0.5 and 1")
+        population_s = float(input("Choose the probability of votes for A from 0.5 to 1: "))
+    population_s= int(population_s*number_population)
     # input smallest probability wanted
-    wantedProb = float(input("Chosse the minumum probability wanted from 0.5 to 1"))
+    wantedProb = float(input("Chosse the minumum probability wanted from 0.5 to 1: "))
+    while (wantedProb<.50 or wantedProb>1):
+        print("Please enter a value between 0.5 and 1")
+        population_s = float(input("Chosse the minumum probability wanted from 0.5 to 1: "))
     print("smallest",findSmallest_2(number_population,population_s,wantedProb))
-    print("smallest",findSmallest_1(number_population,population_s,wantedProb))
-    #print("smallest:",findSmallest_2(number_population,population_s,wantedProb))
-    #print("smallest:",binary_search(number_population,population_s,wantedProb))
-     
-    #print("smallest:",binary_search_updated(number_population,population_s,wantedProb))
+
     printProbability(number_population,population_s)
   
 
